38815eb56eadc356434dfadee9b0d6b9e20037ed
David Blykha <blykhadavid@gmail.com>
2022-07-07 16:22:05 +0300

    mobile code

--- js/index.js
+++ js/index.js
@@ -23,6 +23,40 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
 
-    slidesPerView: 2.1,
-    spaceBetween: 25,
+    breakpoints: {
+      1920: {
+        slidesPerView: 2.1,
+        spaceBetween: 30,
+      },
+
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      576: {
+        slidesPerView: 1,
+        spaceBetween: 60,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 60,
+      }
+    }
   });
+
+  // Burger   ---------------------
+
+ document.querySelector(".header__burger-open").addEventListener("click", function() {
+  document.querySelector(".header__nav").classList.add("header__nav--active");
+})
+document.querySelector(".nav__close").addEventListener("click", function() {
+  document.querySelector(".header__nav").classList.remove("header__nav--active");
+})
